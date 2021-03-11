a0ba64b867fdb54e173b7056bb5d88de7651ee29
Andy Choe <Andrew.s.choe@gmail.com>
2021-03-11 10:29:54 -0500

    completed assign

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
+const isAdmin = obj => obj.userRole === 'ADMIN' 
 
+const getEmail = obj => `${obj.firstName.toLowerCase()}.${obj.lastName.toLowerCase()}@codeimmersives.com`
 
+const getPlaylistLength = obj => obj.songs.length
 
+const getHardestHomework = objects => {
+    if (objects.averageScore === undefined) return '';
+    
+    let lowestScore = objects[0].averageScore;
+    
+    let hwName = '';
+    for (let i = 0; i < objects.length; i++) {
+        if (objects[i].averageScore < lowestScore) {
+            lowestScore = objects[i].averageScore;
+            hwName = objects[i].name
+        }
+    }
+    return hwName;
+}
+
+const createPhonebook = (keys, values) => {   
+    let phoneBook = {};
+
+    for (let i = 0; i < keys.length; i++){       
+        phoneBook[keys[i]] = values[i];
+    }
+    return phoneBook
+}
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
 // └─────────────────────────────────────┘
